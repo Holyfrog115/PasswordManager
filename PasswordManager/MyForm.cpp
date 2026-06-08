54fc9a7c7bb217d9881f66a0f0b83c221fcc5b31
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-06-08 15:13:10 +0700

    Add project files.

---
+++ PasswordManager/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThreadAttribute]
+int main(array<String^>^ args) {
+    Application::EnableVisualStyles();
+    Application::SetCompatibleTextRenderingDefault(false);
+
+    // test — имя вашего проекта, MyForm — имя формы
+    PasswordManager::MyForm form;
+    Application::Run(% form);
+    return 0;
+}
