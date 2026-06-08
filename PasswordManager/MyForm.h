62e67c2f74602d3d04f81f9cb06ae39735fea948
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-06-08 15:27:21 +0700

    Add login and password textboxes

--- PasswordManager/MyForm.h
+++ PasswordManager/MyForm.h
@@ -34,6 +34,8 @@ namespace PasswordManager {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
 
 	private:
 		/// <summary>
@@ -49,18 +51,32 @@ namespace PasswordManager {
 		void InitializeComponent(void)
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 24, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+															  static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(139, 141);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(106, 37);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"label1";
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(584, 561);
+			this->Controls->Add(this->label1);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"MyForm";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
 			this->Text = L"Password Manager";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
